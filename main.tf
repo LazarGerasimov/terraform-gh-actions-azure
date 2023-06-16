d9eb7745cfc1766dd1658fb52a4190d62e2d78d5
LazarGerasimov <99253584+LazarGerasimov@users.noreply.github.com>
2023-06-16 13:32:27 +0100

    add backend azurerm

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ terraform {
       version = "=3.59.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "StorageRG"
+    storage_account_name = "taskboardstoragelaz"
+    container_name = "taskboardcontainer"
+    key = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
